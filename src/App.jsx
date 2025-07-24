cc0b5e941ebc1d00eda183148bbbd5afebd894e5
Lucas Santacruz <lucas.strz2651@gmail.com>
2025-07-24 15:02:18 -0300

    se agrego boton de busqueda y filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import { autos as autosData } from "./data/autos";
 import AutoCard from "./components/AutoCard";
+import Filtro from "./components/Filtro";
+
 
 function App() {
   const [autos, setAutos] = useState(autosData);
+  const [busqueda, setBusqueda] = useState("");
+  const [filtro, setFiltro] = useState("");
+
+    const autosFiltrados = autosData
+      .filter(
+        (auto) =>
+          auto.marca.toLowerCase().includes(busqueda.toLowerCase()) ||
+          auto.modelo.toLowerCase().includes(busqueda.toLowerCase())
+      )
+      .filter((auto) => (filtro ? auto.tipo === filtro : true));
+  
 
   return (
     <div>
       <h1>Catálogo de Autos</h1>
+      <Filtro onBuscar={setBusqueda} onFiltrar={setFiltro} />
+      
       <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-        {autos.map(auto => (
-          <AutoCard key={auto.id} auto={auto} />
+        {autosFiltrados.map(auto => (
+            <AutoCard key={auto.id} auto={auto} />
         ))}
       </div>
     </div>
---
+++ src/components/Filtro.jsx
@@ -0,0 +1,50 @@
+function Filtro({ onBuscar, onFiltrar }) {
+  return (
+    <div
+      style={{
+        marginBottom: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+        gap: "40px",
+        flexWrap: "wrap",
+      }}
+    >
+      <div
+        style={{ display: "flex", flexDirection: "column", minWidth: "250px" }}
+      >
+        <label>
+          <strong>Búsqueda</strong>
+        </label>
+        <input
+          type="text"
+          placeholder="Buscar por marca o modelo"
+          onChange={(e) => onBuscar(e.target.value)}
+          style={{ padding: "8px", fontSize: "1em" }}
+        />
+      </div>
+
+      <div
+        style={{ display: "flex", flexDirection: "column", minWidth: "250px" }}
+      >
+        <label>
+          <strong>Seleccione filtro deseado</strong>
+        </label>
+        <select
+          onChange={(e) => onFiltrar(e.target.value)}
+          defaultValue=""
+          style={{ padding: "8px", fontSize: "1em" }}
+        >
+          <option value="">Todos los tipos</option>
+          <option value="SUV">SUV</option>
+          <option value="Pickup">Pickup</option>
+          <option value="Sedán">Sedán</option>
+          <option value="Eléctrico">Eléctrico</option>
+          <option value="Automática">Automática</option>
+          <option value="Automático">Automático</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Filtro;
